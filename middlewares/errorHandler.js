aa97718c92c08243d4f806caee904ef4efc582e9
jai shukla <shuklajay012@gmail.com>
2023-07-20 16:36:18 +0530

    changes

--- middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-const { CustomError } = require("../errors/customError");
-const ErrorHandler = function (err,req, res, next) {
-  if (err instanceof CustomError) {
-   return res.status(err.statusCode).json({ message: err.message });
-  }
-  res.status(500).json({message:err.message});
-};
-module.exports = ErrorHandler;
